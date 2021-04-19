1d380dfb71311a9141673d5100957fb8618deb72
Hannahvic <hannahvl1987@hotmail.com>
2021-04-19 18:37:03 +0100

    updating about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,20 +14,12 @@ const About = () => {
           className='md:rounded-full md:w-1/6 sm:rounded-lg sm:w-2/6 float-left mr-8 mb-5 md:ml-20'
         />
         <p className='w-6/6 md:ml-20 md:mr-20 lg:mt-8 md:mt-0'>
-          {/* After teaching myself some of the basics of HTML5, CSS3 and JavaScript
+          After teaching myself some of the basics of HTML5, CSS3 and JavaScript
           over a few months I started the Software Development Technician
           Apprenticeship with Pecometer in October 2020. Here I work frontend
           and backend, using mySQL, Typescript, and the backend is an in house
-          framework based on Angular. This portfolio site is built using React
-          with Tailwind CSS. */}
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus
-          consequatur porro aperiam, nobis doloremque eaque reprehenderit
-          asperiores harum fuga corporis rem alias. Nemo nulla asperiores,
-          tempora voluptatum corporis adipisci voluptates! Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Vel voluptates aut ullam,
-          dignissimos officia numquam. Ratione dolore doloremque porro eum et
-          sequi repellendus, sed exercitationem corrupti molestias facilis
-          repudiandae at!
+          framework Pecots. This portfolio site is built using React with
+          Tailwind CSS.
         </p>
       </div>
     </div>
